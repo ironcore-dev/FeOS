caaa2b1d8327ff6a8997832bd09e9b3d60088c82
Lukas Frank <lukas.frank@sap.com>
2024-05-27 16:57:06 +0200

    Download cloud-hypervisor release
    Signed-off-by: Lukas Frank <lukas.frank@sap.com>

--- hack/cloud-hypervisor/make.mk
+++ hack/cloud-hypervisor/make.mk
@@ -1,3 +1,8 @@
+VERSION ?= v39.0
+
+FILENAME = cloud-hypervisor-static-aarch64
+LOCAL_DIR = target/cloud-hypervisor/target
+
 target/cloud-hypervisor:
-	git clone --depth 1 --branch $(shell cat hack/cloud-hypervisor/version) https://github.com/cloud-hypervisor/cloud-hypervisor.git target/cloud-hypervisor
-	docker run --rm -u $${UID} -v "`pwd`:/feos" feos-builder bash -c "cd target/cloud-hypervisor && cargo build --release --target=x86_64-unknown-linux-musl --all"
+	mkdir -p $(LOCAL_DIR)
+	curl -L "https://github.com/cloud-hypervisor/cloud-hypervisor/releases/download/$(URL)/$(FILENAME)" -o "$(LOCAL_DIR)/$(FILENAME)"
--- hack/initramfs/make.mk
+++ hack/initramfs/make.mk
@@ -1,7 +1,7 @@
 initramfs: target/cloud-hypervisor container-release
 	mkdir -p target/rootfs/bin
 	mkdir -p target/rootfs/etc/feos
-	cp target/cloud-hypervisor/target/x86_64-unknown-linux-musl/release/cloud-hypervisor target/rootfs/bin/cloud-hypervisor
+	cp target/cloud-hypervisor/target/cloud-hypervisor-static-aarch64 target/rootfs/bin/cloud-hypervisor
 	cp target/x86_64-unknown-linux-musl/release/feos target/rootfs/bin/feos
 	sudo chown -R `whoami` target/rootfs/etc/feos/
 	cd target/rootfs && rm -f init && ln -s bin/feos init
