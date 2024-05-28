75fc952a34e32d29c4855ad57d594d445cc0a76f
Lukas Frank <lukas.frank@sap.com>
2024-05-28 14:20:12 +0200

    Added ch-firmware
    Signed-off-by: Lukas Frank <lukas.frank@sap.com>

--- hack/cloud-hypervisor/make.mk
+++ hack/cloud-hypervisor/make.mk
@@ -1,6 +1,5 @@
-FILENAME = cloud-hypervisor-static-aarch64
-LOCAL_DIR = target/cloud-hypervisor/target
+CH_FILENAME = cloud-hypervisor-static-aarch64
 
 target/cloud-hypervisor:
-	mkdir -p $(LOCAL_DIR)
-	curl -L "https://github.com/cloud-hypervisor/cloud-hypervisor/releases/download/$(shell cat hack/cloud-hypervisor/version)/$(FILENAME)" -o "$(LOCAL_DIR)/$(FILENAME)"
+	mkdir -p target/cloud-hypervisor/target
+	curl -L "https://github.com/cloud-hypervisor/cloud-hypervisor/releases/download/$(shell cat hack/cloud-hypervisor/version)/$(CH_FILENAME)" -o "target/cloud-hypervisor/target/$(CH_FILENAME)"
