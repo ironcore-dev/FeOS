fdba466a7db9431997f97f3212a3292da3ed1505
Lukas Frank <lukas.frank@sap.com>
2024-05-28 09:52:50 +0200

    PR Review
    Signed-off-by: Lukas Frank <lukas.frank@sap.com>

--- hack/cloud-hypervisor/make.mk
+++ hack/cloud-hypervisor/make.mk
@@ -1,8 +1,6 @@
-VERSION ?= v39.0
-
 FILENAME = cloud-hypervisor-static-aarch64
 LOCAL_DIR = target/cloud-hypervisor/target
 
 target/cloud-hypervisor:
 	mkdir -p $(LOCAL_DIR)
-	curl -L "https://github.com/cloud-hypervisor/cloud-hypervisor/releases/download/$(URL)/$(FILENAME)" -o "$(LOCAL_DIR)/$(FILENAME)"
+	curl -L "https://github.com/cloud-hypervisor/cloud-hypervisor/releases/download/$(shell cat hack/cloud-hypervisor/version)/$(FILENAME)" -o "$(LOCAL_DIR)/$(FILENAME)"
