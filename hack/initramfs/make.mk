788ef58d1ac28f6e7d78b969f4b8af703827832a
Malte Janduda <mail@janduda.net>
2024-11-09 23:25:54 +0000

    adding feos-tui bin

--- hack/initramfs/make.mk
+++ hack/initramfs/make.mk
@@ -5,6 +5,7 @@ initramfs: target/cloud-hypervisor target/cloud-hypervisor-firmware container-re
 	cp target/cloud-hypervisor/target/cloud-hypervisor-static target/rootfs/bin/cloud-hypervisor
 	cp target/cloud-hypervisor/target/hypervisor-fw target/rootfs/usr/share/cloud-hypervisor
 	cp target/x86_64-unknown-linux-musl/release/feos target/rootfs/bin/feos
+	cp target/x86_64-unknown-linux-musl/release/feos-tui target/rootfs/bin/feos-tui
 	sudo chown -R `whoami` target/rootfs/etc/feos/
 	cd target/rootfs && rm -f init && ln -s bin/feos init
 	docker run --rm -u $${UID} -v "`pwd`:/feos" feos-builder bash -c "cd hack/initramfs && ./mk-initramfs"
---
+++ src/bin/feos_tui/main.rs
@@ -0,0 +1,57 @@
+use ratatui::{
+    backend::{CrosstermBackend, Backend},
+    Terminal,
+    widgets::{Block, Borders, Paragraph},
+    text::Span,
+};
+use std::{env, io};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use libc::sprintf;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Set up the terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // Run the app
+    let res = run_app(&mut terminal);
+
+    // Restore the terminal
+    disable_raw_mode()?;
+    execute!(terminal.backend_mut(), LeaveAlternateScreen, DisableMouseCapture)?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        eprintln!("Error: {:?}", err);
+    }
+
+    Ok(())
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
+    loop {
+        terminal.draw(|f| {
+            let size = f.area();
+            let block = Paragraph::new(Span::from("We love Ratatui")).block(
+                Block::default()
+                    .title(format!("FeOS v{}", env!("CARGO_PKG_VERSION")))
+                    .borders(Borders::ALL),
+            );
+
+            f.render_widget(block, size);
+        })?;
+
+        if let Event::Key(key) = event::read()? {
+            if key.code == KeyCode::Char('q') {
+                return Ok(());
+            }
+        }
+    }
+}
