3121314f59ac9e048b6c94807ab7b1af1cdf58e5
Malte Janduda <malte.janduda@sap.com>
2024-11-10 11:51:46 +0100

    updating dependencies (#54)

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
-        .compile(
+        .compile_protos(
             &[
                 "proto/feos.proto",
                 "proto/container.proto",
